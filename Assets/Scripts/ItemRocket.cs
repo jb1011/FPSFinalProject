959e7439b7add95ab82a66f112ab08220ac7d0fe
Jean-Baptiste Demenet <demenet.jb@hotmail.fr>
2020-04-02 14:39:57 +0100

    asset cleaned

---
+++ Assets/Scripts/ItemRocket.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemRocket : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _GuninHand;
+    private void Start()
+    {
+        gameObject.SetActive(true);
+        _GuninHand.SetActive(false);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        gameObject.SetActive(false);
+        _GuninHand.SetActive(true);
+    }
+}
