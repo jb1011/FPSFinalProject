d58729afe6d7e003259fabf931d9f7e4c1616a16
Jean-Baptiste Demenet <demenet.jb@hotmail.fr>
2020-04-02 10:41:14 +0100

    first commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody _characterRb;
+
+    [SerializeField]
+    private float _lookSpeed = 10f;
+
+    private float _camRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float y = Input.GetAxis("Mouse Y");
+        float x = Input.GetAxis("Mouse X");
+
+        _camRotation -= y * _lookSpeed * Time.deltaTime;
+        _camRotation = Mathf.Clamp(_camRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(_camRotation, 0, 0);
+        _characterRb.rotation = Quaternion.Euler(_characterRb.rotation.eulerAngles + x * _lookSpeed * Time.deltaTime * Vector3.up);
+
+        if (Input.GetMouseButton(0))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.Confined;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody _rb;
+    private Vector3 _inputs;
+    private bool _isGrounded = true;
+
+    [SerializeField]
+    private float _jumpForce;
+
+    [SerializeField]
+    private float _originalSpeed;
+
+    [SerializeField]
+    private Transform groundCheckTransform;
+
+    [SerializeField]
+    private LayerMask groundCheckLayerMask;
+
+    [SerializeField]
+    private float _sprint;
+
+    private float _currentSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _inputs = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            _currentSpeed = _sprint;
+        }
+        else
+        {
+            _currentSpeed = _originalSpeed;
+        }
+
+        if(Input.GetKey(KeyCode.Space) && _isGrounded)
+        {
+            _rb.AddForce(_jumpForce * transform.up, ForceMode.Impulse);
+            _isGrounded = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _isGrounded = Physics.CheckSphere(groundCheckTransform.position, 0.3f, groundCheckLayerMask);
+        Vector3 movement = _inputs.z * transform.forward +_inputs.x * transform.right;
+        _rb.MovePosition(_rb.position + movement.normalized * _currentSpeed * Time.fixedDeltaTime);
+    
+    }
+}
