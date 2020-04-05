8752bc77d3569f13dafe17712dcfccd384ad32d6
Jean-Baptiste Demenet <demenet.jb@hotmail.fr>
2020-04-05 17:04:48 +0100

    tree added

--- Assets/Scripts/Level2UIController.cs
+++ Assets/Scripts/Level2UIController.cs
@@ -10,10 +10,14 @@ public class Level2UIController : MonoBehaviour
 
     [SerializeField]
     private TextMeshProUGUI _getInCar;
+
+    [SerializeField]
+    private TextMeshProUGUI _radio;
     // Start is called before the first frame update
     void Start()
     {
         _getInCar.enabled = true;
+        _radio.enabled = false;
     }
 
     // Update is called once per frame
@@ -22,6 +26,11 @@ public class Level2UIController : MonoBehaviour
         if (_isIncar.Value)
         {
             _getInCar.enabled = false;
+            _radio.enabled = true;
+        }
+        else
+        {
+            _radio.enabled = false;
         }
     }
 }
