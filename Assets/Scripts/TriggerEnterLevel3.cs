00c186f9d25355a52a3eb339eb39af93ee2efa5c
Jean-Baptiste Demenet <demenet.jb@hotmail.fr>
2020-04-09 10:46:25 +0100

    waterfall script added

---
+++ Assets/Scripts/TriggerEnterLevel3.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TriggerEnterLevel3 : MonoBehaviour
+{
+    [SerializeField]
+    private IntVariable _killCount;
+
+    [SerializeField]
+    private TextMeshProUGUI _textNotPossible;
+
+    [SerializeField]
+    private TextMeshProUGUI _textGo;
+
+    private void Start()
+    {
+        _textNotPossible.enabled = false;
+        _textGo.enabled = false;
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.CompareTag("Player") && _killCount.Value < 15)
+        {
+            _textNotPossible.enabled = true;
+        }
+
+        if (other.CompareTag("Player") && _killCount.Value >= 15)
+        {
+            _textGo.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _textNotPossible.enabled = false;
+        _textGo.enabled = false;
+    }
+}
