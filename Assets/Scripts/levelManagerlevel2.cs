5b8774fc5f21cb498a406a56eec13315ef9d94be
Jean-Baptiste Demenet <demenet.jb@hotmail.fr>
2020-04-09 11:48:41 +0100

    pause menu added to level 2

--- Assets/Scripts/levelManagerlevel2.cs
+++ Assets/Scripts/levelManagerlevel2.cs
@@ -7,12 +7,61 @@ public class levelManagerlevel2 : MonoBehaviour
 {
     [SerializeField]
     private Transform _player;
-    // Update is called once per frame
+
+    public bool GameIsPaused;
+
+    public GameObject PauseMenuUI;
+
+    //private void Start()
+    //{
+    //    PauseMenuUI.SetActive(false);    
+    //}
+
+    private void Start()
+    {
+        GameIsPaused = false;
+        PauseMenuUI.SetActive(false);
+    }
+
     void Update()
     {
         if(_player.position.y <= -25f)
         {
             SceneManager.LoadScene("Scene02");
-        }   
+        }
+
+        if (Input.GetKey(KeyCode.Escape) && !GameIsPaused)
+        {
+            Pause();
+        }
+
+        if (GameIsPaused && Input.GetKey(KeyCode.Space))
+        {
+            Resume();
+        }
+
+    }
+
+    void Pause()
+    {
+        Cursor.visible = true;
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        AudioListener.pause = true;
+    }
+
+    void Resume()
+    {
+        Cursor.visible = false;
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        AudioListener.pause = false;
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 }
