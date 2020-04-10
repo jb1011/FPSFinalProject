6578ab4dfb70bef28d6ca9b63df9db2161e7ca5b
Jean-Baptiste Demenet <demenet.jb@hotmail.fr>
2020-04-10 09:49:57 +0100

    explosion sound added

--- Assets/CarExplosion.cs
+++ Assets/CarExplosion.cs
@@ -10,10 +10,14 @@ public class CarExplosion : MonoBehaviour
     [SerializeField]
     Transform _spawnExplosion;
 
+    [SerializeField]
+    AudioSource _explosionNoise;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            _explosionNoise.Play();
             Instantiate(_explosion, _spawnExplosion);
         }
     }
