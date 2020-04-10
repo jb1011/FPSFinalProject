64db015f281f0a017265595c67474792713152cf
Jean-Baptiste Demenet <demenet.jb@hotmail.fr>
2020-04-10 09:41:59 +0100

    script car explosion done

---
+++ Assets/CarExplosion.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarExplosion : MonoBehaviour
+{
+    [SerializeField]
+    GameObject _explosion;
+
+    [SerializeField]
+    Transform _spawnExplosion;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Instantiate(_explosion, _spawnExplosion);
+        }
+    }
+}
