4621c076d0172b3dd519ef255fa06b65e64e01ec
Jean-Baptiste Demenet <demenet.jb@hotmail.fr>
2020-04-10 12:14:02 +0100

    lava added

---
+++ Assets/Scripts/Car2ndFloor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car2ndFloor : MonoBehaviour
+{
+    [SerializeField]
+    GameObject _explosion;
+
+    [SerializeField]
+    Transform _spawn;
+
+    [SerializeField]
+    AudioSource _explosionNoise;
+
+    private bool _hasExploded = false;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("Ground") && !_hasExploded)
+        {
+            _hasExploded = true;
+            Instantiate(_explosion, _spawn);
+            _explosionNoise.Play();
+        }
+    }
+}
