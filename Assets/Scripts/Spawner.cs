5ecc4af98baa1e4289e53966e45fc09f75c56216
Jean-Baptiste Demenet <demenet.jb@hotmail.fr>
2020-04-10 13:47:00 +0100

    2nd floor done

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,14 +4,6 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    //[SerializeField]
-    //private GameObject _alien;
-
-    //private int _xPos;
-    //private int _zPos;
-
-    //private int _enemyCount;
-
     [SerializeField]
     private GameObject[] _cars;
 
@@ -23,24 +15,13 @@ public class Spawner : MonoBehaviour
         {
             _car.SetActive(false);
         }
-
-
-        //_cars.SetActive(false);
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.transform.CompareTag("Player") && !_spawned)
         {
             _spawned = true;
-            //while(_enemyCount < 20)
-            //{
-            //    _xPos = Random.Range(-96, -190);
-            //    _zPos = Random.Range(-263, -194);
-            //    Instantiate(_alien, new Vector3(_xPos, 18.5f, _zPos), Quaternion.identity);
-            //    _enemyCount++;
-            //}
 
-            //_car.SetActive(true);
             foreach (GameObject _car in _cars)
             {
                 _car.SetActive(true);
