27dbf67d9454050fc5d0d171547b1006c0e5acad
Jean-Baptiste Demenet <demenet.jb@hotmail.fr>
2020-04-11 16:06:09 +0100

    collider to go upstairs added

---
+++ Assets/Scripts/Level3Manager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level3Manager : MonoBehaviour
+{
+    [SerializeField]
+    private IntVariable _killCount;
+
+    [SerializeField]
+    private GameObject _collider;
+
+    private void Start()
+    {
+        _collider.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_killCount.Value == 12)
+        {
+            _collider.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/UIControllerLevel3.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIControllerLevel3 : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI _killThemAll;
+
+    private float timer = 3.5f;
+
+    void Start()
+    {
+        _killThemAll.enabled = true;
+    }
+
+    void Update()
+    {
+        if(timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        else
+        {
+            _killThemAll.enabled = false;
+        }
+    }
+}
