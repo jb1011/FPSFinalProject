54e4e867ed93cfe4164aadd541e34da1b5b6c105
Jean-Baptiste Demenet <demenet.jb@hotmail.fr>
2020-04-14 16:11:04 +0100

    hurt bonfire

--- Assets/Scripts/BonFire.cs
+++ Assets/Scripts/BonFire.cs
@@ -7,9 +7,16 @@ public class BonFire : MonoBehaviour
 
     [SerializeField]
     private IntVariable _playerHealth;
+
+    [SerializeField]
+    private AudioSource _hurtSound;
+
+    [SerializeField]
+    private Animator _UIController;
     private void OnTriggerStay(Collider other)
     {
         _playerHealth.Value--;
-
+        _hurtSound.Play();
+        _UIController.SetTrigger("PlayerHurt");
     }
 }
