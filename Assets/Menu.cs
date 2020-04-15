9929c794de70d2fe18ccbbd237b9473ee7b433ea
Jean-Baptiste Demenet <demenet.jb@hotmail.fr>
2020-04-15 14:28:19 +0100

    menu added + life enemies reduced

---
+++ Assets/Menu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _mainCanvas;
+
+    [SerializeField]
+    private GameObject _secondCanvas;
+
+    [SerializeField]
+    private AudioSource _button;
+
+    private void Start()
+    {
+        _mainCanvas.SetActive(true);
+        _secondCanvas.SetActive(false);
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene("Scene00");
+        _button.Play();
+    }
+
+    public void Settings()
+    {
+        _mainCanvas.SetActive(false);
+        _secondCanvas.SetActive(true);
+        _button.Play();
+    }
+
+    public void Return()
+    {
+        _mainCanvas.SetActive(true);
+        _secondCanvas.SetActive(false);
+        _button.Play();
+    }
+}
